dd3b3e5bb7ae891fb466385e039d76c6068365dc
AnjanaMadhushan <anjanamadhusha310@gmail.comn>
2024-12-14 16:12:12 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import TitleBar from './components/TitleBar';
+
+import HomePage from './pages/HomePage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+      <h1>this is home page</h1>
+      <TitleBar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TitleBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export default function titleBar() {
+  return (
+    <div>
+      <h1>Title bar</h1>
+      
+    </div>
+  )
+}
